0c17d337c6e6b7aac708644e7008746f6503eac0
Nathan Flessner <nathanflessner1@gmail.com>
2022-01-05 16:36:06 -0700

    inline expressions and interfaces

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-let log = function(message) {
-  console.log(message);
-  
+interface Point {
+  x: number, 
+  y: number
 }
 
-let doLog = (message) => console.log(message);
+let drawPoint = (point: Point) => {
+  // ...
+}
+
+drawPoint({
+  x: 1, 
+  y: 2
+})
