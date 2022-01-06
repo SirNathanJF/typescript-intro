c951e5f42d2ae112fc3e85d1b156aa2d0351b294
Nathan Flessner <nathanflessner1@gmail.com>
2022-01-06 12:57:43 -0700

    modularity

--- main.ts
+++ main.ts
@@ -1,28 +1,4 @@
-export {}
-
-class Point {
-
-  constructor(private _x?: number, private _y?: number) {
-  }
- 
-  draw () {
-    console.log('X: ' + this._x + ', Y: ' + this._y);
-  }
-
-  get x() {
-    return this._x
-  }
-
-  set x(value) {
-    if (value < 0) {
-      throw new Error('Value cannot be less than 0.')
-    } else {
-      this._x = value;
-    }
-  }
-}
+import { Point } from './point'
 
 let point = new Point(1, 2);
 point.draw();
-let x = point.x;
-point.x = 10;
